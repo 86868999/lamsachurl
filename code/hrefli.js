3b5e44904fd9dba542ae19f146b07a6e5078affd
86868999 <94746512+86868999@users.noreply.github.com>
2022-07-14 11:31:56 +0700

    Update hrefli.js

--- code/hrefli.js
+++ code/hrefli.js
@@ -3,4 +3,5 @@
 * Xóa chuyển hướng href.li của Tumlbr: https://son.li/z6Bi
 * Chia sẻ bởi Lâm Thái Sơn: https://son.lt */
 
+/* Xóa chuyển hướng href.li của Tumlbr */
 function RemoveHrefLi(){var e=document.querySelectorAll('a[href*="href.li/?"]');Array.prototype.forEach.call(e,(function(r,t){var f=e[t].getAttribute("href").split("href.li/?")[1];e[t].setAttribute("href",f)}))}RemoveHrefLi()

5e50064f6dac0a78c7d9272f0b8fcebe521a756b
86868999 <94746512+86868999@users.noreply.github.com>
2022-07-14 11:28:41 +0700

    Update hrefli.js

--- code/hrefli.js
+++ code/hrefli.js
@@ -1,9 +1,6 @@
 /* Làm sạch URL: https://lamsachurl.thaison.page
-
 * Thủ thuật về Tumblr: https://thuthuatve.tumblr.com
-
 * Xóa chuyển hướng href.li của Tumlbr: https://son.li/z6Bi
-
 * Chia sẻ bởi Lâm Thái Sơn: https://son.lt */
 
 function RemoveHrefLi(){var e=document.querySelectorAll('a[href*="href.li/?"]');Array.prototype.forEach.call(e,(function(r,t){var f=e[t].getAttribute("href").split("href.li/?")[1];e[t].setAttribute("href",f)}))}RemoveHrefLi()
